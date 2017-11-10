d1c3508cda99beb641c13f06bc94d227f0996622
Michael Matranga <mamatranga@gmail.com>
2017-11-10 13:15:53 -0500

    Initial commit.

---
+++ ObjectTracker/ViewController.swift
@@ -0,0 +1,103 @@
+import AVFoundation
+import Vision
+import UIKit
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet private weak var cameraView: UIView?
+    @IBOutlet private weak var highlightView: UIView? {
+        didSet {
+            self.highlightView?.layer.borderColor = UIColor.red.cgColor
+            self.highlightView?.layer.borderWidth = 4
+            self.highlightView?.backgroundColor = .clear
+        }
+    }
+    
+    private let visionSequenceHandler = VNSequenceRequestHandler()
+    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    private lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        if session.canSetSessionPreset(.hd1920x1080) {
+            session.beginConfiguration()
+            session.sessionPreset = .hd1920x1080
+            session.commitConfiguration()
+        }
+
+        guard
+            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: backCamera)
+        else { return session }
+        session.addInput(input)
+        return session
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.highlightView?.frame = .zero
+        self.cameraView?.layer.addSublayer(self.cameraLayer)
+
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "cameraQueue"))
+
+        self.captureSession.addOutput(videoOutput)
+        self.captureSession.startRunning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.cameraLayer.frame = self.cameraView?.bounds ?? .zero
+    }
+    
+    private var lastObservation: VNDetectedObjectObservation?
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard
+            let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
+            let lastObservation = self.lastObservation
+        else { return }
+        
+        let request = VNTrackObjectRequest(detectedObjectObservation: lastObservation, completionHandler: self.handleVisionRequestUpdate)
+        request.trackingLevel = .accurate
+        
+        do {
+            try self.visionSequenceHandler.perform([request], on: pixelBuffer)
+        } catch {
+            print("Throws: \(error)")
+        }
+    }
+    
+    private func handleVisionRequestUpdate(_ request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let newObservation = request.results?.first as? VNDetectedObjectObservation else { return }
+            self.lastObservation = newObservation
+            
+            guard newObservation.confidence >= 0.3 else {
+                self.highlightView?.frame = .zero
+                return
+            }
+
+            var transformedRect = newObservation.boundingBox
+            transformedRect.origin.y = 1 - transformedRect.origin.y
+            let convertedRect = self.cameraLayer.layerRectConverted(fromMetadataOutputRect: transformedRect)
+            self.highlightView?.frame = convertedRect
+        }
+    }
+    
+    @IBAction private func userTapped(_ sender: UITapGestureRecognizer) {
+        self.highlightView?.frame.size = CGSize(width: 120, height: 120)
+        self.highlightView?.center = sender.location(in: self.view)
+
+        let originalRect = self.highlightView?.frame ?? .zero
+        var convertedRect = self.cameraLayer.metadataOutputRectConverted(fromLayerRect: originalRect)
+        convertedRect.origin.y = 1 - convertedRect.origin.y
+        
+        let newObservation = VNDetectedObjectObservation(boundingBox: convertedRect)
+        self.lastObservation = newObservation
+    }
+    
+    @IBAction private func resetTapped(_ sender: UIBarButtonItem) {
+        self.lastObservation = nil
+        self.highlightView?.frame = .zero
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# objectTracker
+ObjectTracking using Vision on iOS 11
+
+Bare bones object tracking.
+
+Must have iOS 11 installed.
